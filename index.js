2fabe456c7995217b4f8afdd07463c679bbcf53b
Jtrd123 <06604@sukhon.ac.th>
2025-10-20 21:32:50 +0700

    static page

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const router = require('./routes/myRouter');
+const path = require('path');
 const app = express()
-
-app.use('/', router);
+app.use(express.static(path.join(__dirname,'public')))
 
 app.listen(8080,()=>{
     console.log("Server is running on port 8080");
