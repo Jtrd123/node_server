c8712a4242dcc8d9223f2294948a6e84439ae22a
Jtrd123 <06604@sukhon.ac.th>
2025-10-21 00:43:40 +0700

    template

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -1,5 +1,16 @@
 const express = require('express');
-const path = require('path');
 const router = express.Router();
+
+router.get('/',(req,res)=>{
+  const products = [
+    {name: "Notebook", price: 48900, image:"../images/products/product1.png"},
+    {name: "Clothes", price: 3300, image:"../images/products/product2.png"},
+    {name: "Headphones", price: 8900, image:"../images/products/product3.png"}
+
+  ]
+  res.render('index',{
+      products: products  
+  })
+})
 
 module.exports = router;
